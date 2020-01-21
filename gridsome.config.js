cafc2d778eba8587d9536693076527e8f71b30a7
Charles E. Amyx, III <amyx@umich.edu>
2020-01-21 14:33:14 -0500

    add intital gridsome files and starter theme

---
+++ gridsome.config.js
@@ -0,0 +1,87 @@
+module.exports = {
+  siteName: 'Gridsome',
+
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true,
+        shouldPurgeUnusedKeyframes: true,
+      }
+    },
+    {
+      use: 'gridsome-source-static-meta',
+      options: {
+        path: 'settings/*.json'
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Author',
+        path: './content/author/*.md'
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Blog',
+        path: './content/blog/**/*.md',
+        refs: {
+          author: 'Author',
+          tags: {
+            typeName: 'Tag',
+            create: true
+          },
+          category: {
+            typeName: 'Category',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: 'gridsome-plugin-pwa',
+      options: {
+          title: 'Gridsome',
+          startUrl: '/',
+          display: 'standalone',
+          statusBarStyle: 'default',
+          manifestPath: 'manifest.json',
+          disableServiceWorker: true,
+          serviceWorkerPath: 'service-worker.js',
+          cachedFileTypes: 'js,json,css,html,png,jpg,jpeg,svg',
+          shortName: 'Gridsome',
+          themeColor: '#666600',
+          backgroundColor: '#ffffff',
+          icon: '', // must be provided
+          msTileImage: '',
+          msTileColor: '#666600'
+      }
+  }
+  ],
+  templates: {
+    Blog: [{
+      path: '/blog/:title',
+      component: './src/templates/BlogEntry.vue'
+    }],
+    Category: [{
+      path: '/category/:title',
+      component: './src/templates/Category.vue'
+    }],
+    Tag: [{
+      path: '/tag/:title',
+      component: './src/templates/Tag.vue'
+    }]
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set('@modules', '@/resources/js/modules');
+    config.resolve.alias.set('@utilities', '@/resources/js/utilities');
+    
+  }
+}
