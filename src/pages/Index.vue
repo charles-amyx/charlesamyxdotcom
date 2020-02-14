117db3cc3ad5a8513a586fa4f59cbf8045dd7b04
Charles E. Amyx <charles@Charless-MacBook-Pro.local>
2020-02-13 23:43:10 -0500

    increase font size of the one and only p tag

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,7 +3,7 @@
     <section id="container-centre" class="flex-1 column centre">
       <h1 class="text-gray-500 page-title">Digital art director, painter, photographer</h1>
       <div class="content">
-        <p class="text-base tracking-wide text-gray-600">
+        <p class="text-xl tracking-wide text-gray-500">
           Currently at the University of Michigan. 
         </p>
       </div>
