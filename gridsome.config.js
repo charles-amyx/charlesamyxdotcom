c5dcb4decfce64a30e5cd45896d9d24b6a42f2b8
Charles E. Amyx <charles@Charless-MacBook-Pro.local>
2020-02-16 12:18:12 -0500

    turn off critical to test if the mobile bug goes away

--- gridsome.config.js
+++ gridsome.config.js
@@ -75,14 +75,14 @@ module.exports = {
         lang: "en",
       },
     },
-    {
-      use: '@gridsome/plugin-critical',
-      options: {
-        paths: ['/'],
-        width: 1300,
-        height: 900
-      }
-    },
+    // {
+    //   use: '@gridsome/plugin-critical',
+    //   options: {
+    //     paths: ['/'],
+    //     width: 1300,
+    //     height: 900
+    //   }
+    // },
     {
       use: '@gridsome/plugin-google-analytics',
       options: {
