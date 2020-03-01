53cbe8abb88b5f05ad64baae8b13af483e6fe731
Charles E. Amyx <amyx@umich.edu>
2020-03-01 15:29:56 -0500

    update photos with alt text

--- src/pages/Photo.vue
+++ src/pages/Photo.vue
@@ -3,15 +3,15 @@
     <section class="flex-1">
       <h1 class="mb-4 text-3xl page-title md:text-center md:text-5xl lg:text-6xl">Photography</h1>
       <p class="mb-16 text-base text-gray-400 lg:mb-24 md:text-center">Light bends.</p>
-      <ClientOnly>
-        <div v-masonry transition-duration="0.3s" item-selector=".item" :fit-width="true">
-          <div v-masonry-tile class="item" v-for="(item,i) in items" :key="i">
-            <div class="m-3 overflow-hidden bg-white rounded shadow-lg max-w-22">
-              <g-image class="w-full" :src="item.src" />
-            </div>
+      <div v-masonry transition-duration="0.3s" item-selector=".item" :fit-width="true">
+        <div v-masonry-tile class="item" v-for="(item,i) in items" :key="i">
+          <div class="m-3 overflow-hidden bg-white rounded shadow-lg max-w-22">
+            <figure>
+              <g-image class="block loaded" :src="item.src" :alt="item.caption" />
+            </figure>
           </div>
         </div>
-      </ClientOnly>
+      </div>
     </section>
   </Layout>
 </template>
@@ -40,64 +40,84 @@ export default {
     return {
       items: [
         {
-          src: require("../assets/img/1.jpg")
+          src: require("../assets/img/1.jpg"),
+          caption: "Vilnius LT"
         },
         {
-          src: require("../assets/img/2.jpg")
+          src: require("../assets/img/2.jpg"),
+          caption: "Vilnius LT and BMW"
         },
         {
-          src: require("../assets/img/3.jpg")
+          src: require("../assets/img/3.jpg"),
+          caption: "Vilnius LT and tourists"
         },
         {
-          src: require("../assets/img/4.jpg")
+          src: require("../assets/img/4.jpg"),
+          caption: "Vilnius LT and a couple of tourist"
         },
         {
-          src: require("../assets/img/5.jpg")
+          src: require("../assets/img/5.jpg"),
+          caption: "Varena LT in a field"
         },
         {
-          src: require("../assets/img/6.jpg")
+          src: require("../assets/img/6.jpg"),
+          caption: "Varena LT on a lake in a secret location"
         },
         {
-          src: require("../assets/img/7.jpg")
+          src: require("../assets/img/7.jpg"),
+          caption: "Varena LT in a boat on a lake in a secret location"
         },
         {
-          src: require("../assets/img/8.jpg")
+          src: require("../assets/img/8.jpg"),
+          caption: "Vilnius LT in Uzupis"
         },
         {
-          src: require("../assets/img/9.jpg")
+          src: require("../assets/img/9.jpg"),
+          caption: "Vilnius LT in Uzupis"
         },
         {
-          src: require("../assets/img/10.jpg")
+          src: require("../assets/img/10.jpg"),
+          caption: "english setter"
         },
         {
-          src: require("../assets/img/11.jpg")
+          src: require("../assets/img/11.jpg"),
+          caption: "gordon setter"
         },
         {
-          src: require("../assets/img/12.jpg")
+          src: require("../assets/img/12.jpg"),
+          caption: "a trail in the woods"
         },
         {
-          src: require("../assets/img/13.jpg")
+          src: require("../assets/img/13.jpg"),
+          caption: "two elk"
         },
         {
-          src: require("../assets/img/14.jpg")
+          src: require("../assets/img/14.jpg"),
+          caption: "english setter"
         },
         {
-          src: require("../assets/img/15.jpg")
+          src: require("../assets/img/15.jpg"),
+          caption: "a child smiling"
         },
         {
-          src: require("../assets/img/16.jpg")
+          src: require("../assets/img/16.jpg"),
+          caption: "children on a merry go round"
         },
         {
-          src: require("../assets/img/17.jpg")
+          src: require("../assets/img/17.jpg"),
+          caption: "a grown adult on the monkey bars"
         },
         {
-          src: require("../assets/img/18.jpg")
+          src: require("../assets/img/18.jpg"),
+          caption: "a hunter in a field"
         },
         {
-          src: require("../assets/img/19.jpg")
+          src: require("../assets/img/19.jpg"),
+          caption: "flandreau, sd"
         },
         {
-          src: require("../assets/img/20.jpg")
+          src: require("../assets/img/20.jpg"),
+          caption: "a child in the snow"
         }
       ]
     };
