9c0f980cdbdd1b98e85b56deec846ddebbaf5adb
Charles E. Amyx, III <amyx@umich.edu>
2020-06-30 11:44:50 -0400

    fix: Begin initial work to fix photo grid

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,60 +1,58 @@
-const tailwind = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')
+const tailwind = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
 
-const postcssPlugins = [
-  tailwind(),
-]
+const postcssPlugins = [tailwind()];
 
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
+if (process.env.NODE_ENV === "production") postcssPlugins.push(purgecss());
 
 module.exports = {
-  siteName: 'C. E. Amyx, III',
-  siteUrl: 'https://charlesamyx.com',
+  siteName: "C. E. Amyx, III",
+  siteUrl: "https://charlesamyx.com",
   plugins: [
     {
-      use: 'gridsome-plugin-tailwindcss',
+      use: "gridsome-plugin-tailwindcss",
       options: {
-        tailwindConfig: './tailwind.config.js',
+        tailwindConfig: "./tailwind.config.js",
         purgeConfig: {},
         presetEnvConfig: {},
         shouldPurge: true,
         shouldImport: true,
         shouldTimeTravel: true,
         shouldPurgeUnusedKeyframes: true,
-      }
+      },
     },
     {
-      use: 'gridsome-source-static-meta',
+      use: "gridsome-source-static-meta",
       options: {
-        path: 'settings/*.json'
-      }
+        path: "settings/*.json",
+      },
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        typeName: 'Design',
-        path: './content/design/**/*.md',
+        typeName: "Design",
+        path: "./content/design/**/*.md",
         refs: {
           category: {
-            typeName: 'Category',
-            create: true
-          }
-        }
-      }
+            typeName: "Category",
+            create: true,
+          },
+        },
+      },
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        typeName: 'Paint',
-        path: './content/paint/**/*.md'
-      }
+        typeName: "Paint",
+        path: "./content/paint/**/*.md",
+      },
     },
     {
-      use: '@gridsome/source-filesystem',
+      use: "@gridsome/source-filesystem",
       options: {
-        typeName: 'Photo',
-        path: './content/photo/**/*.md'
-      }
+        typeName: "Photo",
+        path: "./content/photo/**/*.md",
+      },
     },
     {
       use: "gridsome-plugin-service-worker",
@@ -78,62 +76,62 @@ module.exports = {
         short_name: "CEAmyxIII",
         theme_color: "#284052",
         lang: "en",
-        start_url: "/"
+        start_url: "/",
       },
     },
     {
-      use: '@gridsome/plugin-google-analytics',
+      use: "@gridsome/plugin-google-analytics",
       options: {
-        id: 'UA-8525758-2',
+        id: "UA-8525758-2",
         debug: {
-          sendHitTask: process.env.NODE_ENV === 'production'
-        }
-      }
+          sendHitTask: process.env.NODE_ENV === "production",
+        },
+      },
     },
     {
-      use: 'gridsome-plugin-robots-txt',
+      use: "gridsome-plugin-robots-txt",
       options: {
-        host: 'https://charlesamyx.com',
-        sitemap: 'https://charlesamyx/sitemap.xml',
+        host: "https://charlesamyx.com",
+        sitemap: "https://charlesamyx/sitemap.xml",
         policy: [
           {
             userAgent: "Googlebot",
             allow: "/",
             disallow: "/search",
-            crawlDelay: 2
+            crawlDelay: 2,
           },
           {
             userAgent: "*",
             allow: "/",
             disallow: "/search",
             crawlDelay: 10,
-            cleanParam: "ref /design/"
-          }
-        ]
-      }
+            cleanParam: "ref /design/",
+          },
+        ],
+      },
     },
     {
-      use: '@gridsome/plugin-sitemap',
+      use: "@gridsome/plugin-sitemap",
       options: {
         cacheTime: 600000, // default
-        exclude: ['/contact'],
+        exclude: ["/contact"],
         config: {
-          '/design/*': {
-            changefreq: 'weekly',
-            priority: 0.5
+          "/design/*": {
+            changefreq: "weekly",
+            priority: 0.5,
           },
-          '/about': {
-            changefreq: 'monthly',
-            priority: 0.7
-          }
-        }
-      }
+          "/about": {
+            changefreq: "monthly",
+            priority: 0.7,
+          },
+        },
+      },
     },
   ],
   transformers: {
     remark: {
-      externalLinksTarget: false
-    }
+      externalLinksTarget: false,
+    },
   },
   css: {
     loaderOptions: {
@@ -143,13 +141,20 @@ module.exports = {
     },
   },
   templates: {
-    Design: [{
-      path: '/design/:title',
-      component: './src/templates/Entry.vue'
-    }],
-    Category: [{
-      path: '/category/:title',
-      component: './src/templates/Category.vue'
-    }]
+    Design: [
+      {
+        path: "/design/:title",
+        component: "./src/templates/Entry.vue",
+      },
+    ],
+    Category: [
+      {
+        path: "/category/:title",
+        component: "./src/templates/Category.vue",
+      },
+    ],
   },
-}
+  chainWebpack: (config) => {
+    config.resolve.alias.set("@images", "../assets/img");
+  },
+};
