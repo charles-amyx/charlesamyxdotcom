5f07f25e4565ce5143b42d791f093bc658c865c2
Charles E. Amyx, III <amyx@umich.edu>
2020-08-20 22:41:42 -0400

    fix: Update links on home page to g-links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,30 +1,30 @@
 <template>
-  <Layout>
-    <section class="flex-1">
-      <div class="font-serif leading-tight text-gray-300 page-title">
-        <a
-          class="inline-block text-gray-400 transition hover:text-indigo-400 alpha focus:text-indigo-400"
-          href="/design/"
-        >designer</a>,
-        <a
-          class="inline-block text-gray-400 transition alpha hover:text-indigo-400 focus:text-indigo-400"
-          href="/paint/"
-        >painter</a>,
-        <a
-          class="inline-block text-gray-400 transition alpha hover:text-indigo-400 focus:text-indigo-400"
-          href="/photo/"
-        >photographer</a>
-      </div>
-      <div class="content">
-        <p class="text-lg tracking-wide">
-          <a
-            class="pb-1 text-gray-400 transition border-b border-transparent hover:text-indigo-400 focus:text-indigo-400 hover:border-indigo-400"
-            href="https://creative.umich.edu"
-          >Currently at the University of Michigan.</a>
-        </p>
-      </div>
-    </section>
-  </Layout>
+    <Layout>
+        <section class="flex-1">
+            <div class="font-serif leading-tight text-gray-300 page-title">
+                <g-link
+                    class="inline-block text-gray-400 transition hover:text-indigo-400 alpha focus:text-indigo-400"
+                    to="/design/"
+                >designer</g-link>,
+                <g-link
+                    class="inline-block text-gray-400 transition alpha hover:text-indigo-400 focus:text-indigo-400"
+                    to="/paint/"
+                >painter</g-link>,
+                <g-link
+                    class="inline-block text-gray-400 transition alpha hover:text-indigo-400 focus:text-indigo-400"
+                    to="/photo/"
+                >photographer</g-link>
+            </div>
+            <div class="content">
+                <p class="text-lg tracking-wide">
+                    <a
+                        class="pb-1 text-gray-400 transition border-b border-transparent hover:text-indigo-400 focus:text-indigo-400 hover:border-indigo-400"
+                        href="https://creative.umich.edu"
+                    >Currently at the University of Michigan.</a>
+                </p>
+            </div>
+        </section>
+    </Layout>
 </template>
 
 <script>
