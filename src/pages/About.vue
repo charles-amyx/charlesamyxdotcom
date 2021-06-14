b88ee25f7c820604a782761d629ef94d30c5aaa1
amyx <amyx@umich.edu>
2021-06-14 13:58:05 -0400

    fix: Correct typo in about

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -19,7 +19,7 @@
                             interest around inclusive, accessible, and
                             compassionate web design.
                             <br />
-                            <br />Proud Filipino American, escrimador, and fly fisherman.
+                            <br />Proud Filipino American, eskrimador, and fly fisherman.
                             I also played guitar in a number of bands in Florida no
                             one's ever heard of.
                         </p>
