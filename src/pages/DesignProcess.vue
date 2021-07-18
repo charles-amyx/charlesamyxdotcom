367fcd14873d0a01b1d7a175a5d930d1fd3e9690
amyx <ceamyx@gmail.com>
2021-07-17 20:04:25 -0400

    content: Add design process page template

---
+++ src/pages/DesignProcess.vue
@@ -0,0 +1,24 @@
+<template>
+    <Layout>
+        <div class="w-full">
+            <section class="flex-1 w-full py-10 mx-auto max-w-75 lg:py-24">
+                <div class="items-center -mx-6 md:flex">
+                    <div class="w-full px-6 md:w-2/5">
+                        <h1 class="mb-0 text-3xl font-normal leading-none md:text-4xl lg:text-6xl">Process</h1>
+                        <hr class="mb-12" />
+                        <p>A little more detail on the design process I've been working with using a recent project.</p>
+                    </div>
+                </div>
+            </section>
+            My final design project at Michigan Creative. The client wanted a more modern platform for their data and research. 
+        </div>
+    </Layout>
+</template>
+
+<script>
+export default {
+    metaInfo: {
+        title: "Design process detail",
+    },
+};
+</script>
