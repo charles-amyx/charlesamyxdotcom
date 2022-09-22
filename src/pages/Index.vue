5a50257b535842085cc99f84c94810685608aa82
Charles E.Amyx, III <ceamyx@gmail.com>
2022-09-22 11:58:16 -0400

    fix: fix line height issue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,11 +3,13 @@
     <section class="flex-1 w-full max-w-6xl px-6 ml-auto mr-auto">
       <div class="mb-8 font-normal text-black font-mega lg:mb-16 page-title">
         <h1 class="text-4xl leading-snug md:text-7xl">
-          <g-link
-            class="text-gray-800 transition hover:text-indigo-700 alpha focus:text-indigo-700"
-            to="/design/"
-            >Designer
-          </g-link>
+          <p class="mb-6 text-4xl leading-snug md:text-7xl">
+            <g-link
+              class="text-gray-800 transition hover:text-indigo-700 alpha focus:text-indigo-700"
+              to="/design/"
+              >Designer
+            </g-link>
+          </p>
           <p class="mb-6 text-4xl leading-snug md:text-7xl">
             <g-link
               class="text-gray-800 transition hover:text-indigo-700 alpha focus:text-indigo-700"
