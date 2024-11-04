a2d27fa77b26c8198a0909c0b3a3bfa4007f13e4
Charles E.Amyx, III <ceamyx@gmail.com>
2024-11-04 10:32:15 -0500

    chore: fix build errors

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface Window {
+  Alpine: any;
+}
+
+declare module '@alpinejs/intersect' {
+  const intersect: any;
+  export default intersect;
+}
